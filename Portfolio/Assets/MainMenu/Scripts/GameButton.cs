b28d044dd83140f71de8bbc6ba01525b50589d4a
Aleksandr Shevchuk <explogeek@gmail.com>
2019-12-02 23:13:42 +0300

    implement basic Game Button + reorganize files

---
+++ Portfolio/Assets/MainMenu/Scripts/GameButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+[RequireComponent(typeof(Button))]
+[ExecuteInEditMode]
+public class GameButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameButtonData gameButtonData = null;
+
+    private Button button;
+
+    private void OnEnable()
+    {
+        if (gameButtonData != null)
+           SetupButton();
+    }
+
+    private void SetupButton()
+    {
+        button = GetComponent<Button>();
+        if (gameButtonData.GamePreviewImage != null)
+            button.image = gameButtonData.GamePreviewImage;
+        var buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = gameButtonData.GameName;
+        button.onClick.AddListener(() => SceneManager.LoadScene(gameButtonData.SceneName));
+    }
+}
---
+++ Portfolio/Assets/MainMenu/Scripts/GameButtonData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Game Button Data", menuName = "Main Menu/Game Button Data")]
+public class GameButtonData : ScriptableObject
+{
+    public string SceneName = null;
+    public string GameName;
+    public Image GamePreviewImage;
+}
