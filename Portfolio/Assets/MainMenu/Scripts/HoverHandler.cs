2c04b040f9a70ac16089946b9520de26fcdc42e6
Aleksandr Shevchuk <explogeek@gmail.com>
2019-12-03 17:42:12 +0300

    fix not being able to scroll with mouse over GameButtons
    implemented HoverHandler that doesn't care about drag events instead of EventTrigger that did capture drag events

---
+++ Portfolio/Assets/MainMenu/Scripts/HoverHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class HoverHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private UnityEvent PointerEnterAction;
+    [SerializeField]
+    private UnityEvent PointerExitAction;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        PointerEnterAction.Invoke();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        PointerExitAction.Invoke();
+    }
+}
