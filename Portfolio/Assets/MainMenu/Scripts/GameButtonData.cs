1440395baf4efb801a9c55553c94162343a1255b
Aleksandr Shevchuk <explogeek@gmail.com>
2019-12-03 18:35:26 +0300

    add link buttons
    - link buttons to FB, HH
    - removed unused using statements
    - file management

--- Portfolio/Assets/MainMenu/Scripts/GameButtonData.cs
+++ Portfolio/Assets/MainMenu/Scripts/GameButtonData.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "Game Button Data", menuName = "Main Menu/Game Button Data")]
---
+++ Portfolio/Assets/MainMenu/Scripts/UrlOpener.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class UrlOpener : MonoBehaviour
+{
+    [SerializeField]
+    private string url = "";
+
+    public void OpenUrl()
+    {
+        Application.OpenURL(url);
+    }
+}
