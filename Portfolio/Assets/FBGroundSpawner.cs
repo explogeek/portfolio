9cb7704ebb0431ee87c407de2bb0bdf8f485db44
Aleksandr Shevchuk <explogeek@gmail.com>
2019-12-05 22:22:13 +0300

    add ground spawner

---
+++ Portfolio/Assets/FBGroundSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBGroundSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject groundPrefab = null;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag != "Ground")
+            return;
+        Transform transform = collision.gameObject.transform;
+        float width = collision.gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+        // TODO: Pooling
+        float xPosition = transform.position.x + width;
+        var spawnPosition = new Vector2(xPosition, transform.position.y);
+        Instantiate(groundPrefab, spawnPosition, transform.rotation);
+    }
+}
