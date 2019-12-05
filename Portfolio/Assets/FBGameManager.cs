17e4fb23c3fd4d3469c7db2068b3d9e07a0bc63a
Aleksandr Shevchuk <explogeek@gmail.com>
2019-12-05 20:58:11 +0300

    add player + add basic game logic
    - added player (flappy bird) with animation
    - jumps with Jump button
    - player sends out events on getting past gate or hitting anything else
    - stop movement on player hit
    - flipped sprite for top pipe
    - sprite sorting added

---
+++ Portfolio/Assets/FBGameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBGameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        FBPlayer.OnHit += GameOver;
+        FBPlayer.OnGatePass += IncreaseScore;
+    }
+
+
+    private void OnDestroy()
+    {
+        FBPlayer.OnHit -= GameOver;
+        FBPlayer.OnGatePass -= IncreaseScore;
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("Game over!");
+        FBMover.Stop();
+    }
+
+    private void IncreaseScore()
+    {
+        Debug.Log("Increase score");
+    }
+}
---
+++ Portfolio/Assets/Games/FlappyBird/Scripts/FBPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class FBPlayer : MonoBehaviour
+{
+
+    public static event Action OnHit = delegate { };
+    public static event Action OnGatePass = delegate { };
+
+    [SerializeField]
+    [Range(100, 800)]
+    private int jumpForce = 250;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<FBMover>() != null)
+            OnGatePass();
+        else
+            OnHit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = rb.velocity / 10;
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+}
