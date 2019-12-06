8ef69fe36f39f457780e72815a8d72d4ae5275c6
Aleksandr Shevchuk <explogeek@gmail.com>
2019-12-06 16:46:07 +0300

    add background spawning
    also change name of FBGroundSpawner to FBBackgroundSpawner

--- Portfolio/Assets/Games/FlappyBird/Scripts/FBBackgroundSpawner.cs
+++ Portfolio/Assets/Games/FlappyBird/Scripts/FBBackgroundSpawner.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class FBGroundSpawner : MonoBehaviour
+public class FBBackgroundSpawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject groundPrefab = null;
+    private GameObject prefab = null;
+    [SerializeField]
+    private string prefabTag = "bgSpawnable";
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag != "Ground")
+        if (collision.tag != prefabTag)
             return;
         Transform transform = collision.gameObject.transform;
         float width = collision.gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
         // TODO: Pooling
         float xPosition = transform.position.x + width;
         var spawnPosition = new Vector2(xPosition, transform.position.y);
-        Instantiate(groundPrefab, spawnPosition, transform.rotation);
+        Instantiate(prefab, spawnPosition, transform.rotation, this.gameObject.transform);
     }
 }
