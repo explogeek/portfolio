d56a1aff2abef8e64894596827fbc96c01a77bdd
Aleksandr Shevchuk <explogeek@gmail.com>
2019-12-12 17:46:47 +0300

    add pause manager

---
+++ Portfolio/Assets/FBInputManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class FBInputManager : MonoBehaviour
+{
+    public UnityEvent cancelPressed;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            cancelPressed.Invoke();
+        }
+    }
+}
---
+++ Portfolio/Assets/Games/Common/Scripts/PauseManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseManager : MonoBehaviour
+{
+    public UnityEvent paused, resumed;
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        Resume();
+    }
+
+    public void Toggle()
+    {
+        if (isPaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        paused.Invoke();
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        resumed.Invoke();
+    }
+}
